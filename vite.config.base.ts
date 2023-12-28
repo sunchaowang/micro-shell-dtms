acd55e2d0483f1a00026513bfbb6a4ca2ced08c0
sunchaowang <sunchaowang@outlook.com>
2023-12-28 18:07:16 +0800

    优化包项目结构

--- vite.config.base.ts
+++ vite.config.base.ts
@@ -1,5 +1,6 @@
 import { UserConfig } from 'vite';
-
+import vue from '@vitejs/plugin-vue';
+import vueJs from '@vitejs/plugin-vue-jsx';
 import { loadEnv } from 'vite';
 import { resolve } from 'path';
 
@@ -12,8 +13,16 @@ export const root = process.cwd();
 export const loadPackageEnv = (mode: string) => loadEnv(mode, root);
 
 export const userConfig: UserConfig = {
+  plugins: [vue(), vueJs()],
   resolve: {},
   server: {
     host: true,
+  },
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+      },
+    },
   },
 };
