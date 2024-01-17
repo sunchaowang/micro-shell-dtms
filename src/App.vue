4d8748b598a3f1ec44ea988f40236873614c37f2
sunchaowang <sunchaowang@outlook.com>
2024-01-17 18:00:25 +0800

    优化项目结构

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+  import { ref } from 'vue';
+  const count = ref(0);
+</script>
+<template>
+  <div>
+    <h1>Home</h1>
+    <p>Count is: {{ count }}</p>
+    <a-button type="primary" @click="count++">count++</a-button>
+  </div>
+</template>
+
+<style scoped lang="less"></style>
---
+++ vite.config.ts
@@ -0,0 +1,75 @@
+import { loadEnv } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueJs from '@vitejs/plugin-vue-jsx';
+import { resolve } from 'path';
+import { ViteEnv } from '@/libs/types';
+import Components from 'unplugin-vue-components/vite';
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
+
+/**
+ * 项目根目录下的相对路径
+ * @param dir
+ */
+export function pathResolve(dir: string) {
+  return resolve(process.cwd(), '.', dir);
+}
+
+// 获取根路径
+export const root = process.cwd();
+
+/**
+ *  读取环境变量
+ * @param mode 'development' | 'production'
+ * @param rootPath 根目录 默认root
+ * @returns
+ */
+export const loadC = (mode: string, rootPath?: string) => loadEnv(mode, rootPath ?? root);
+
+export default ({ command, mode }) => {
+  const { VITE_APP_PORT } = loadEnv(mode, process.cwd()) as unknown as ViteEnv;
+  return {
+    plugins: [
+      vue({
+        template: {
+          compilerOptions: {
+            isCustomElement: (tag) => /^micro-app/.test(tag),
+          },
+        },
+      }),
+      vueJs(),
+      Components({
+        resolvers: [AntDesignVueResolver()],
+      }),
+    ],
+    resolve: {
+      alias: [
+        // @shared
+        {
+          find: '@/',
+          replacement: pathResolve('src/'),
+        },
+        // @root
+        {
+          find: '@root/',
+          replacement: pathResolve('./'),
+        },
+      ],
+    },
+    server: {
+      port: VITE_APP_PORT,
+      host: true,
+    },
+    css: {
+      preprocessorOptions: {
+        less: {
+          javascriptEnabled: true,
+        },
+      },
+    },
+    rollupOptions: {
+      output: {
+        comment: false,
+      },
+    },
+  };
+};
